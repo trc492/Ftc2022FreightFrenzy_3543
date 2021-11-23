a54d4b2818f291ce06f7e19f97cbe3643ddd28ef
trc492 <trc492@live.com>
2021-11-23 01:38:45 -0800

    Added ExclusiveSubsystem support to Intake. Added TestOpMode to test the Intake subsystem.

--- Intake.java
+++ Intake.java
@@ -24,57 +24,176 @@ package Ftc2022FreightFrenzy_3543;
 
 import TrcCommonLib.trclib.TrcAnalogSensorTrigger;
 import TrcCommonLib.trclib.TrcEvent;
+import TrcCommonLib.trclib.TrcExclusiveSubsystem;
+import TrcCommonLib.trclib.TrcNotifier;
 import TrcCommonLib.trclib.TrcTimer;
 import TrcFtcLib.ftclib.FtcDcMotor;
 import TrcFtcLib.ftclib.FtcDistanceSensor;
 
-class Intake extends FtcDcMotor
+/**
+ * This class implements an auto-assist intake subsystem. It contains a motor and a distance sensor that detects if
+ * the intake has picked up freight. It provides the pickupFreight method that allows the caller to call the intake
+ * subsystem to pickup freight on a press of a button and the intake subsystem will stop itself once freight is
+ * detected in the intake. While it provides the auto-assist functionality to pickup freight, it also supports
+ * exclusive subsystem access by implementing TrcExclusiveSubsystem. This enables the intake subsystem to be aware
+ * of multiple callers access to the subsystem. While one caller starts the intake for an operation, nobody can
+ * access it until the previous caller is done with the operation.
+ */
+class Intake extends FtcDcMotor implements TrcExclusiveSubsystem
 {
     private static final String moduleName = "Intake";
-    private static final double FREIGHT_THRESHOLD = 3.0;
+    private static final double FREIGHT_THRESHOLD = 3.8;    //in cm
     private static final double[] thresholds = {FREIGHT_THRESHOLD};
     private final FtcDistanceSensor sensor;
     private final TrcAnalogSensorTrigger<FtcDistanceSensor.DataType> distanceTrigger;
     private final TrcTimer timer;
     private TrcEvent onFinishEvent = null;
+    private TrcNotifier.Receiver onFinishCallback = null;
+    private String ownerID = null;
 
+    /**
+     * Constructor: Creates an instance of the object.
+     *
+     * @param instanceName
+     */
     public Intake(String instanceName)
     {
         super(instanceName);
         sensor = new FtcDistanceSensor(moduleName + "Sensor");
         distanceTrigger = new TrcAnalogSensorTrigger<>(
-            moduleName + "SensorTrigger", sensor, 0, FtcDistanceSensor.DataType.DISTANCE_INCH, thresholds,
+            moduleName + "SensorTrigger", sensor, 0, FtcDistanceSensor.DataType.DISTANCE_CM, thresholds,
             this::triggerHandler, false);
         timer = new TrcTimer(moduleName + "Timer");
     }   //Intake
 
-    public void pickUpFreight(double power, TrcEvent event, double timeout)
+    /**
+     * This method spins the intake with the given power. It will only do this if nobody acquires exclusive access
+     * to the intake already.
+     *
+     * @param power specifies the power value to spin the intake.
+     */
+    @Override
+    public void set(double power)
     {
-        if (hasFreight())
+        if (hasOwnership(null))
         {
-            event.signal();
-        }
-        else
-        {
+            //
+            // Nobody has ownership, go ahead to take over.
+            //
             super.set(power);
-            this.onFinishEvent = event;
-            timer.set(timeout, this::timeoutHandler);
-            distanceTrigger.setEnabled(true);
+        }
+    }
+
+    /**
+     * This method is an auto-assist operation. It allows the caller to start the intake spinning at the given power
+     * and it will stop itself once freight is detected in the intake at which time the given event will be signaled
+     * or it will notify the caller's handler.
+     *
+     * @param owner specifies the owner ID to check if the caller has ownership of the intake subsystem.
+     * @param power specifies the power value to spin the intake.
+     * @param event specifies the event to signal when there is freight detected in the intake.
+     * @param callback specifies the callback handler to call when there is freight detected in the intake.
+     * @param timeout specifies a timeout value at which point it will give up and signal completion. The caller
+     *                must call hasFreight to figure out if it has given up.
+     */
+    public void pickupFreight(String owner, double power, TrcEvent event, TrcNotifier.Receiver callback, double timeout)
+    {
+        //
+        // This is an auto-assist pickup, make sure the caller has ownership.
+        //
+        if (validateOwnership(owner))
+        {
+            ownerID = owner;
+
+            if (hasFreight())
+            {
+                if (event != null)
+                {
+                    event.signal();
+                }
+
+                if (callback != null)
+                {
+                    callback.notify(ownerID);
+                }
+
+                ownerID = null;
+            }
+            else
+            {
+                super.set(power);
+                this.onFinishEvent = event;
+                this.onFinishCallback = callback;
+                if (timeout > 0.0)
+                {
+                    timer.set(timeout, this::timeoutHandler);
+                }
+                distanceTrigger.setEnabled(true);
+            }
         }
     }   //pickupFreight
 
+    /**
+     * This method is an auto-assist operation. It allows the caller to start the intake spinning at the given power
+     * and it will stop itself once freight is detected in the intake.
+     *
+     * @param owner specifies the owner ID to check if the caller has ownership of the intake subsystem.
+     * @param power specifies the power value to spin the intake.
+     */
+    public void pickupFreight(String owner, double power)
+    {
+        pickupFreight(owner, power, null, null, 0.0);
+    }   //pickupFreight
+
+    /**
+     * This method reads the distance value from the distance sensor.
+     *
+     * @return distance value read from the distance sensor.
+     */
+    public double getDistance()
+    {
+        return sensor.getRawData(0, FtcDistanceSensor.DataType.DISTANCE_CM).value;
+    }   //getDistance
+
+    /**
+     * This method checks if there is freight in the intake.
+     *
+     * @return true if there is freight in the intake, false otherwise.
+     */
     public boolean hasFreight()
     {
-        return sensor.getRawData(0, FtcDistanceSensor.DataType.DISTANCE_INCH).value > FREIGHT_THRESHOLD;
+        return getDistance() < FREIGHT_THRESHOLD;
     }   //hasFreight
 
+    /**
+     * This method is called internally either at the end of the timeout or when freight is detected to stop the
+     * intake's spinning and signal or notify the caller for completion.
+     */
     private void cancel()
     {
         super.set(0.0);
-        onFinishEvent.signal();
+
+        if (onFinishEvent != null)
+        {
+            onFinishEvent.signal();
+        }
+
+        if (onFinishCallback != null)
+        {
+            onFinishCallback.notify(ownerID);
+        }
+
+        ownerID = null;
         distanceTrigger.setEnabled(false);
     }   //cancel
 
+    /**
+     * This method is called when freight is detected in the intake.
+     *
+     * @param currZone specifies the current threshold zone.
+     * @param prevZone specifies the previous threshold zone.
+     * @param zoneValue specifies the sensor value.
+     */
     private void triggerHandler(int currZone, int prevZone, double zoneValue)
     {
         if (currZone < prevZone)
@@ -84,6 +203,11 @@ class Intake extends FtcDcMotor
         }
     }   //triggerHandler
 
+    /**
+     * This method is called when timeout expires.
+     *
+     * @param timer specifies the timer that has expired.
+     */
     private void timeoutHandler(Object timer)
     {
         cancel();
---
+++ TestOpMode.java
@@ -0,0 +1,84 @@
+package Ftc2022FreightFrenzy_3543;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import TrcCommonLib.trclib.TrcEvent;
+import TrcCommonLib.trclib.TrcGameController;
+import TrcFtcLib.ftclib.FtcDashboard;
+import TrcFtcLib.ftclib.FtcGamepad;
+import TrcFtcLib.ftclib.FtcOpMode;
+
+@TeleOp(name="FtcTestOp", group="Ftc3543")
+public class TestOpMode extends FtcOpMode
+{
+    FtcDashboard dashboard;
+    FtcGamepad gamepad;
+    Intake intake;
+    TrcEvent event;
+
+    @Override
+    public void initRobot()
+    {
+        dashboard = FtcDashboard.getInstance();
+        gamepad = new FtcGamepad("GamePad", gamepad1, this::gamepadButtonEvent);
+        gamepad.setYInverted(true);
+        intake = new Intake("intakeMotor");
+        event = new TrcEvent("intakeEvent");
+        event.signal();
+    }   //initRobot
+
+    @Override
+    public void runPeriodic(double elapsedTime)
+    {
+        if (event.isSignaled())
+        {
+            intake.set(gamepad.getRightStickY(true));
+        }
+        dashboard.displayPrintf(1, "Distance: %.3f cm", intake.getDistance());
+    }   //runPeriodic
+
+    public void gamepadButtonEvent(TrcGameController gamepad, int button, boolean pressed)
+    {
+        final String ownerID = "autoAssistPickup";
+
+        dashboard.displayPrintf(
+            7, "%s: %04x->%s", gamepad, button, pressed? "Pressed": "Released");
+        switch (button)
+        {
+            case FtcGamepad.GAMEPAD_A:
+                if (pressed && intake.acquireExclusiveAccess(ownerID))
+                {
+                    event.clear();
+                    intake.pickupFreight(ownerID, -1.0, null, this::intakeCallback, 0.0);
+                }
+                break;
+
+            case FtcGamepad.GAMEPAD_B:
+                break;
+
+            case FtcGamepad.GAMEPAD_X:
+                break;
+
+            case FtcGamepad.GAMEPAD_Y:
+                break;
+
+            case FtcGamepad.GAMEPAD_LBUMPER:
+                break;
+
+            case FtcGamepad.GAMEPAD_RBUMPER:
+                break;
+
+            case FtcGamepad.GAMEPAD_DPAD_UP:
+                break;
+
+            case FtcGamepad.GAMEPAD_DPAD_DOWN:
+                break;
+        }
+    }   //gamepadButtonEvent
+
+    private void intakeCallback(Object owner)
+    {
+        intake.releaseExclusiveAccess((String) owner);
+    }   //intakeCallback
+
+}   //class TestOpMode
